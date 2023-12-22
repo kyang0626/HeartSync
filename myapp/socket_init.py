66d9f1ac91436e41768ff7c3b05f13e4e007f11b
Kong Yang <kong_yang828@hotmail.com>
2023-12-22 14:22:39 -0500

    added cors_allowed_origin value

--- myapp/socket_init.py
+++ myapp/socket_init.py
@@ -1,4 +1,4 @@
 from flask_socketio import SocketIO
 
 # Initialize SocketIO with the app
-socketio = SocketIO(cors_allowed_origins='*')
+socketio = SocketIO(cors_allowed_origins=['http://heartsync.onrender.com/matches', 'https://heartsync.onrender.com/matches'])
