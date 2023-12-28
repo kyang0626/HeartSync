ffdb805eb684dfa5dc74ce1816abad131e789251
Kong Yang <kong_yang828@hotmail.com>
2023-12-28 09:54:26 -0500

    socketio cors_origins value update

--- myapp/socket_init.py
+++ myapp/socket_init.py
@@ -1,4 +1,4 @@
 from flask_socketio import SocketIO
 
 # Initialize SocketIO with the app
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO(cors_allowed_origins="https://heartsync.onrender.com")
