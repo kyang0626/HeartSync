32e449a038070b2f1a04fa5dbc7e6b2d7edd8f57
Kong Yang <kong_yang828@hotmail.com>
2024-01-03 14:19:25 -0500

    change socket connection to domain name instead of default

--- myapp/static/scripts/index.js
+++ myapp/static/scripts/index.js
@@ -202,7 +202,7 @@ function moreInfo() {
 
 
 // LISTENERS
-const socket = io.connect('http://127.0.0.1:5000');
+const socket = io.connect('https://heartsync.onrender.com/');
 
 // Test Connection
 socket.on("connect", () => {
